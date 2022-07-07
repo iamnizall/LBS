39e25c207b76b8b82c5940a4e6fc44e19ddde24f
amiminn <kochengpoi@gmail.com>
2022-07-07 10:46:21 +0700

    start

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>- {{ name }}</h1>
+    <button class="btn btn-primary">ok</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "home",
+    };
+  },
+};
+</script>
---
+++ src/views/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <router-link to="/">Home {{ name }}</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "ok",
+    };
+  },
+};
+</script>
