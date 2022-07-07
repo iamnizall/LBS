127168de6845ba42dc7849b4db9b04d74f4d3b4e
amiminn <kochengpoi@gmail.com>
2022-07-07 16:38:55 +0700

    set nav

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -11,113 +11,51 @@
     <!-- Material Design Bootstrap -->
     <link rel="stylesheet" href="css/mdb.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <style>
-        html,
-        body,
-        header,
-        .view {
-            height: 100%;
-        }
-
-        @media (max-width: 740px) {
-
-            html,
-            body,
-            header,
-            .view {
-                height: 1040px;
-            }
-        }
-
-        @media (min-width: 800px) and (max-width: 850px) {
-
-            html,
-            body,
-            header,
-            .view {
-                height: 600px;
-            }
-        }
-    </style>
 </head>
 
-<body>
+<body style="background-color: white ">
     <header>
         @include('./components/Navbar')
-        <div class="mb-1">
-            <section>
-                <!--Carousel Wrapper-->
-                <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
-                    <!--Indicators-->
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-2" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-2" data-slide-to="2"></li>
-                    </ol>
-                    <!--/.Indicators-->
-                    <!--Slides-->
-                    <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active">
-                            <div class="view">
-                                <img class="d-block w-100"
-                                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp" alt="First slide">
-                                <div class="mask rgba-black-light"></div>
-                            </div>
-                            <div class="carousel-caption">
-                                <h3 class="h3-responsive">Light mask</h3>
-                                <p>First text</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <!--Mask color-->
-                            <div class="view">
-                                <img class="d-block w-100"
-                                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp" alt="Second slide">
-                                <div class="mask rgba-black-strong"></div>
-                            </div>
-                            <div class="carousel-caption">
-                                <h3 class="h3-responsive">Strong mask</h3>
-                                <p>Secondary text</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <!--Mask color-->
-                            <div class="view">
-                                <img class="d-block w-100"
-                                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp" alt="Third slide">
-                                <div class="mask rgba-black-slight"></div>
-                            </div>
-                            <div class="carousel-caption">
-                                <h3 class="h3-responsive">Slight mask</h3>
-                                <p>Third text</p>
-                            </div>
-                        </div>
+        <main class="text-center py-5 mt-3">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            exercitation
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea voluptates dignissimos incidunt
+                            vero cum voluptate, odit vitae eos aliquam ad eum, porro nam mollitia atque recusandae
+                            ipsam. Fuga blanditiis consequuntur, consectetur vitae neque autem aspernatur at, dolorem
+                            voluptate facilis corrupti repudiandae! Nobis cum quod dignissimos dolorum! Blanditiis id
+                            earum alias. Voluptatum nostrum ipsam accusamus quo, necessitatibus amet facere? Odit
+                            accusamus iure dolorum eos possimus, repudiandae tempora, soluta delectus natus cupiditate
+                            quae est eum, excepturi nemo eaque reprehenderit! Soluta deserunt veniam, praesentium,
+                            dolores aut possimus illo veritatis inventore accusamus ipsam alias corrupti suscipit,
+                            distinctio incidunt deleniti? Error suscipit obcaecati doloremque rem!</p>
+
                     </div>
-                    <!--/.Slides-->
-                    <!--Controls-->
-                    <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                    <!--/.Controls-->
                 </div>
-                <!--/.Carousel Wrapper-->
-            </section>
-        </div>
-    </header>
-    <h1>sec 2</h1>
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="js/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="js/mdb.min.js"></script>
-    @stack('script')
+            </div>
+
+        </main>
+
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="js/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="js/mdb.min.js"></script>
+        @stack('script')
 </body>
 
 </html>
--- resources/views/components/Navbar.blade.php
+++ resources/views/components/Navbar.blade.php
@@ -1,23 +1,83 @@
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar top-nav-collapse">
-    <div class="container">
-        <a class="navbar-brand" href="#"><strong>Preview</strong></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7"
-            aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+<header>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+        <a class="navbar-brand" href="#"><strong>Navbar</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse fload-rigth" id="navbarSupportedContent-7">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link waves-effect waves-light" href="#">Home <span
-                            class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link waves-effect waves-light" href="#">Link</a>
+                    <a class="nav-link" href="#">Link</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link waves-effect waves-light" href="#">Profile</a>
+                    <a class="nav-link" href="#">Profile</a>
                 </li>
             </ul>
         </div>
+    </nav>
+
+    <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+        <!--Indicators-->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-2" data-slide-to="1"></li>
+            <li data-target="#carousel-example-2" data-slide-to="2"></li>
+        </ol>
+        <!--/.Indicators-->
+        <!--Slides-->
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <div class="view">
+                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).webp"
+                        alt="First slide">
+                    <div class="mask rgba-black-light"></div>
+                </div>
+                <div class="carousel-caption">
+                    <h3 class="h3-responsive">Light mask</h3>
+                    <p>First text</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <!--Mask color-->
+                <div class="view">
+                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).webp"
+                        alt="Second slide">
+                    <div class="mask rgba-black-strong"></div>
+                </div>
+                <div class="carousel-caption">
+                    <h3 class="h3-responsive">Strong mask</h3>
+                    <p>Secondary text</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <!--Mask color-->
+                <div class="view">
+                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp"
+                        alt="Third slide">
+                    <div class="mask rgba-black-slight"></div>
+                </div>
+                <div class="carousel-caption">
+                    <h3 class="h3-responsive">Slight mask</h3>
+                    <p>Third text</p>
+                </div>
+            </div>
+        </div>
+        <!--/.Slides-->
+        <!--Controls-->
+        <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        <!--/.Controls-->
     </div>
-</nav>
+
+</header>
