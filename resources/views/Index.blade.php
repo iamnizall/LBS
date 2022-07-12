59f56ee959a3cf4732c254aeb38e1dc2eaa9dbdf
iamnizall <nizalrizqi69@gmail.com>
2022-07-12 13:24:56 +0700

    add

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -46,6 +46,16 @@
                 </div>
             </div>
 
+            <!--Google map-->
+            <div class="container">
+
+                <div id="map-container-google-2" class="z-depth-1-half map-container" style="height: 500px">
+                    <iframe src="https://maps.google.com/maps?q=chicago&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+                    style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+  
+  <!--Google Maps-->
         </main>
 
         <script src="js/jquery-3.4.1.min.js"></script>
--- resources/views/components/Navbar.blade.php
+++ resources/views/components/Navbar.blade.php
@@ -6,18 +6,21 @@
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Profile</a>
-                </li>
-            </ul>
+        <div class="float-right">
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
