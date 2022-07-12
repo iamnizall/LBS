d86b72dda28d0f4d2c1eb6d19cf28067c7db257d
amiminn <kochengpoi@gmail.com>
2022-07-12 13:13:25 +0700

    nw section

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -16,36 +16,10 @@
 <body style="background-color: white ">
     <header>
         @include('./components/Navbar')
-        <main class="text-center py-5 mt-3">
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-
-                        <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation
-                            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non
-                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea voluptates dignissimos incidunt
-                            vero cum voluptate, odit vitae eos aliquam ad eum, porro nam mollitia atque recusandae
-                            ipsam. Fuga blanditiis consequuntur, consectetur vitae neque autem aspernatur at, dolorem
-                            voluptate facilis corrupti repudiandae! Nobis cum quod dignissimos dolorum! Blanditiis id
-                            earum alias. Voluptatum nostrum ipsam accusamus quo, necessitatibus amet facere? Odit
-                            accusamus iure dolorum eos possimus, repudiandae tempora, soluta delectus natus cupiditate
-                            quae est eum, excepturi nemo eaque reprehenderit! Soluta deserunt veniam, praesentium,
-                            dolores aut possimus illo veritatis inventore accusamus ipsam alias corrupti suscipit,
-                            distinctio incidunt deleniti? Error suscipit obcaecati doloremque rem!</p>
-
-                    </div>
-                </div>
-            </div>
-
+        <main>
+            <section>
+                @include('./components/Generate')
+            </section>
         </main>
 
         <script src="js/jquery-3.4.1.min.js"></script>
