3241c5c9bfc2eaa8167ad419f7d24827a5a1b5ee
amiminn <kochengpoi@gmail.com>
2022-07-14 12:20:27 +0700

    mark location

--- resources/views/components/Generate.blade.php
+++ resources/views/components/Generate.blade.php
@@ -1,7 +1,7 @@
 <div class="container mb-3">
     <div class="text-center mt-3">
         <button class="btn btn-primary focus">
-            <i class="fas fa-search"></i> My Location
+            <i class="fas fa-search"></i> Lokasi saya
         </button>
         <div class="alert alert-success" hidden>
             <h5>
@@ -12,79 +12,104 @@
     <div class="row">
         <div class="col-lg-8 mt-3">
             <div class="card">
-                <div id="map" style="width: 100%; height: 400px"></div>
+                <div id="app" style="width: 100%; height: 400px"></div>
             </div>
         </div>
         <div class="col-lg-4 mt-3">
-            <ul class="list-group">
-                <li
-                    class="list-group-item d-flex justify-content-between align-items-center"
-                >
-                    Pantai Parangtritis
-                    <span class="badge badge-primary badge-pill">1,4 km</span>
-                </li>
-                <li
-                    class="list-group-item d-flex justify-content-between align-items-center"
-                >
-                    Pantai Parangkusumo
-                    <span class="badge badge-primary badge-pill">2,3 km</span>
-                </li>
-                <li
-                    class="list-group-item d-flex justify-content-between align-items-center"
-                >
-                    Hutan Pinus Mangunan sampai Puncak Becici
-                    <span class="badge badge-primary badge-pill">5 km</span>
-                </li>
+            <ul class="list-group" id="list">
             </ul>
         </div>
     </div>
 </div>
 
 @push('script')
+
 <script>
-    $(".focus").click(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-            $(".focus").html("...");
-        } else {
-            console.log("Geolocation is not supported by this browser.");
-        }
-    });
+    function lokasiWisata(){
+        let data = [
+            { tempatWisata : 'Pantai Parangtritis', jarak : 1.4},
+            { tempatWisata : 'Pantai Parangkusumo', jarak : 2.3},
+            { tempatWisata : 'Hutan Pinus Mangunan sampai Puncak Becici', jarak : 2.1 },
+        ]
 
-    function showPosition(position) {
-        const LAT = position.coords.latitude;
-        const LNG = position.coords.longitude;
-        console.log(LAT, LNG);
-        myMap(LAT, LNG);
-        $.get(
-            `https://api.opencagedata.com/geocode/v1/json?q=${LAT}+${LNG}&key=597956fbef70415fb8801a4c13cb3493`,
-            (res) => {
-                const result = res.results[0].formatted;
-                $(".alert").removeAttr("hidden");
-                $("#kota").html(`${result}`);
-                $(".focus").html("<i class='fas fa-search'></i> my Location");
-            }
-        );
+        let listTempat = ''
+        data.map((d)=>{
+            listTempat += `<li class="list-group-item d-flex justify-content-between align-items-center">${d.tempatWisata}<span class="badge badge-primary badge-pill">${d.jarak} km</span></li>`
+      
+        })
+        $('#list').html(listTempat)
     }
 
-    function myMap(LAT, LNG) {
-        if (LAT == null) {
-            var mapProp = {
-                center: new google.maps.LatLng(-7.797068, 110.370529),
-                zoom: 12,
-            };
-        } else {
-            var mapProp = {
-                center: new google.maps.LatLng(LAT, LNG),
-                zoom: 15,
-            };
-        }
-        var map = new google.maps.Map(document.getElementById("map"), mapProp);
-    }
+    lokasiWisata()
 </script>
 
-<script
-    src="https://maps.googleapis.com/maps/api/js?callback=myMap&v=weekly"
-    defer
-></script>
+<script>
+    $(".focus").click(() => {
+        $(".focus").html("...");
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const tourStops = [
+          [{ lat: position.coords.latitude, lng: position.coords.longitude }, "Posisi Sekarang"],
+          [{ lat: -7.782984, lng: 110.367035 }, "Tugu Yogyakkarta"],
+          [{ lat: -7.750026, lng: 110.402501 }, "Jogja Amazon Green"],
+        ];
+
+        let pos = position.coords;
+        initMap(pos.latitude, pos.longitude, tourStops);
+
+        $.get(
+          `https://api.opencagedata.com/geocode/v1/json?q=${pos.latitude}+${pos.longitude}&key=597956fbef70415fb8801a4c13cb3493`,
+          (res) => {
+            const result = res.results[0].formatted;
+            $(".alert").removeAttr("hidden");
+            $("#kota").html(`${result}`);
+            $(".focus").html("<i class='fas fa-search'></i> Lokasi saya");
+          }
+        );
+      });
+    });
+
+    function initMap(LAT, LNG, tourStops) {
+      if (LAT == null) {
+        var mapProp = {
+          center: new google.maps.LatLng(-7.797068, 110.370529),
+          zoom: 12,
+        };
+      } else {
+        var mapProp = {
+          center: new google.maps.LatLng(LAT, LNG),
+          zoom: 12,
+        };
+      }
+      const map = new google.maps.Map(
+        document.getElementById("app"),
+        mapProp
+      );
+
+      if (tourStops) {
+        setMarker(tourStops)
+      }
+
+      function setMarker(tourStops){
+        const infoWindow = new google.maps.InfoWindow();
+
+        tourStops.forEach(([position, title], i) => {
+          const marker = new google.maps.Marker({
+            position,
+            map,
+            title: `${title}`,
+            optimized: false,
+          });
+
+          marker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent(marker.getTitle());
+            infoWindow.open(marker.getMap(), marker);
+          });
+        });
+      }
+    }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?&callback=initMap"></script>
+
 @endpush
